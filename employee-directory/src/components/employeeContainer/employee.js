72b4a7c7eb07fe5ccf1e64f449040a552696e1f0
stevie2codes <stevie2codes@gmail.com>
2020-03-17 14:49:22 -0500

    created json objects to workwith instead of the broken random api and implemented them into table

--- employee-directory/src/components/employeeContainer/employee.js
+++ employee-directory/src/components/employeeContainer/employee.js
@@ -1,28 +1,58 @@
-import api from "../../utils/api";
+// import api from "../../utils/api";
 import React, { Component } from "react";
-import Table from "../table/table";
 import Header from "../header/header";
+import { getEmployees } from "../../employeeData";
+import "./employee.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 class Employee extends Component {
   state = {
-    results: []
+    employees: getEmployees()
+    // results: []
   };
 
-  componentDidMount() {
-    this.searchEmployee();
-  }
+  // componentDidMount() {
+  //   this.searchEmployee();
+  // }
 
-  searchEmployee = query => {
-    api
-      .search(query)
-      .then(res => this.setState({ results: res.data.results }))
-      .catch(err => console.log(err));
-  };
+  // searchEmployee = query => {
+  //   api
+  //     .search(query)
+  //     .then(res => this.setState({ results: res.data.results }))
+  //     .catch(err => console.log(err));
+  // };
   render() {
+    const { length: count } = this.state.employees;
     return (
       <React.Fragment>
         <Header />
-        <Table className="listContainer" name={this.state.results.length} />
+        <p className="employeeCount">Showing {count} Employees in the DB</p>
+        <table className="table table-dark ">
+          <thead className="head">
+            <tr className="trow">
+              <th scope="col">Avatar</th>
+              <th scope="col">Name</th>
+              <th scope="col">Email</th>
+              <th scope="col">Location</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.employees.map(employee => (
+              <tr key={employee._id}>
+                <td>
+                  <img className="avatar" src={employee.picture} alt="avatar" />
+                </td>
+                <td>
+                  {employee.title}
+                  {employee.name.first}
+                  {employee.name.last}
+                </td>
+                <td>{employee.email}</td>
+                <td>{employee.location}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </React.Fragment>
     );
   }
---
+++ employee-directory/src/employeeData.js
@@ -0,0 +1,79 @@
+const employees = [
+  {
+    _id: "5b21ca3eeb7f6fbccd471815",
+    title: "Mr. ",
+    name: { first: "Stephen", last: " Webb" },
+    age: 34,
+    email: "Stevo@example.com",
+    location: "Nashville,Tn",
+    picture: "https://picsum.photos/id/7/55/55"
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471816",
+    title: "Mr. ",
+    name: { first: "Robert ", last: " paulsen" },
+    email: "hisnamewas@example.com",
+    location: "Detriot, MI",
+    picture: "https://picsum.photos/id/50/55/55"
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471817",
+    title: "Mrs. ",
+    name: { first: "Jennifer", last: " Smith" },
+    email: "smith@example.com",
+    location: "Orlando, FL",
+    picture: "https://picsum.photos/id/9/55/55"
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471ui9",
+    title: "Mrs. ",
+    name: { first: "Chloe", last: " Fuson" },
+    email: "chloe@example.com",
+    location: "Austin, Tx",
+    picture: "https://picsum.photos/id/90/55/55"
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd4718tr9",
+    title: "Mrs. ",
+    name: { first: "Sarah", last: " Johnson" },
+    email: "Johnson@example.com",
+    location: "Dallas, Tx",
+    picture: "https://picsum.photos/id/95/55/55"
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471ww9",
+    title: "Mrs. ",
+    name: { first: "Karlie", last: " Green" },
+    email: "Green@example.com",
+    location: "Seattle, WA",
+    picture: "https://picsum.photos/id/1005/55/55"
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471832",
+    title: "Mr. ",
+    name: { first: "David", last: " Myers" },
+    email: "David@example.com",
+    location: "Peoria, IL",
+    picture: "https://picsum.photos/id/2/55/55"
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471855",
+    title: "Mr. ",
+    name: { first: "Joe", last: " Rogan" },
+    email: "Rogan@example.com",
+    location: "Los Angeles,CA",
+    picture: "https://picsum.photos/id/40/55/55"
+  },
+  {
+    _id: "5b21ca3eeb7f6fbccd471833",
+    title: "Mr. ",
+    name: { first: "Donnie", last: " Brasco" },
+    email: "theDon@example.com",
+    location: "Queens, NY",
+    picture: "https://picsum.photos/id/10/55/55"
+  }
+];
+
+export function getEmployees() {
+  return employees;
+}
